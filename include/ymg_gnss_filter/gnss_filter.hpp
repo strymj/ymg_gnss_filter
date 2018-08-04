2b2f80d91c3ed94bb59e86f9b84340abeb3c4890
strymj <yamaji.shoutarou@gmail.com>
2018-08-04 14:07:04 +0900

    update

--- include/ymg_gnss_filter/gnss_filter.hpp
+++ include/ymg_gnss_filter/gnss_filter.hpp
@@ -1,22 +1,51 @@
 #pragma once
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/Pose2D.h>
 #include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <nmea_msgs/Sentence.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/TwistStamped.h>
 
 class GnssFilter {
 	public:
 		GnssFilter();
 		void spin();
+		void twistCallback( const geometry_msgs::TwistStamped::ConstPtr& msg );
+		void pointCallback( const geometry_msgs::PointStamped::ConstPtr& msg );
+		void doFiltering( const geometry_msgs::PoseStamped::ConstPtr& msg );
 	private:
-		// void doFiltering();
-		void updatePoselist();
+		void removeOldPose( const ros::Time stamp );
+		void motionUpdate( const ros::Time stamp );
+		void updatePoselist( const geometry_msgs::PoseStamped& msg );
+		void publishParticleCloud( const std_msgs::Header header );
+		void publishPose();
+		void broadcastTF();
+		std_msgs::Header header_;
 		std::string odom_frame_ = "odom";
-		std::string robot_frame_ = "base_link";
+		std::string gnss_frame_ = "gnss";
+		std::string robot_frame_ = "base_link_yp";
+		std::string robot_gnss_frame_ = "base_link";
+
+		Eigen::Vector2d gnss_mean;
+		double gnss_sd;   // dtandard deviation
+		int min_store_n_ = 20;
+		double store_sec_ = 10.0;
+		double sd_threshold_ratio_ = 1.0;
     tf2_ros::Buffer tf2_buffer_;
     tf2_ros::TransformListener tf2_listener_;
-		ros::Publisher robot_pose_publisher_;
-		ros::Subscriber gnss_subscriber_;
-		std::vector<geometry_msgs::Pose> poselist_;
+		ros::Publisher robot_pose_publisher_, pose_cloud_publisher_;
+    ros::Subscriber gnss_fix_subscriber_, gnss_twist_subscriber_, gnss_point_subscriber_;
+		geometry_msgs::Pose2D robot_pose2d_;
+		struct Pose2dWithTime {
+			ros::Time timestamp;
+			geometry_msgs::Pose2D pose2d;
+		};
+		std::vector<Pose2dWithTime> poselist_;
 }; // class GnssFilter
--- src/gnss_filter.cpp
+++ src/gnss_filter.cpp
@@ -1,31 +1,76 @@
 #include <ymg_gnss_filter/gnss_filter.hpp>
+#include <Eigen/Geometry>
 
 GnssFilter::GnssFilter()
 	: tf2_listener_( tf2_buffer_ )
 {/*{{{*/
 	ros::NodeHandle nh, private_nh("~");
 	private_nh.param("odom_frame", odom_frame_, odom_frame_ );
+	private_nh.param("odom_gnss_frame", robot_gnss_frame_, robot_gnss_frame_ );
 	private_nh.param("robot_frame", robot_frame_, robot_frame_ );
+	private_nh.param("min_store_n", min_store_n_, min_store_n_ );
+	private_nh.param("store_sec", store_sec_, store_sec_ );
+	private_nh.param("sd_threshold_ratio", sd_threshold_ratio_, sd_threshold_ratio_ );
 
 	robot_pose_publisher_ = nh.advertise<geometry_msgs::PoseStamped>( "gnss_robot_pose", 1 );
-	gnss_subscriber_ = nh.subscribe( "move_base_simple/goal", 100, &GnssFilter::doFiltering, this );
+	pose_cloud_publisher_ = nh.advertise<geometry_msgs::PoseArray>( "gnss_robot_cloud", 1 );
+	// gnss_fix_subscriber_ = nh.subscribe( "/fix", 100, &GnssFilter::doFiltering, this );
+	gnss_twist_subscriber_ = nh.subscribe( "/vel", 100, &GnssFilter::twistCallback, this );
+	gnss_point_subscriber_ = nh.subscribe( "/gnss_point", 100, &GnssFilter::pointCallback, this );
+}/*}}}*/
+
+void GnssFilter::spin()
+{/*{{{*/
+	ros::spin();
+}/*}}}*/
+
+void GnssFilter::twistCallback( const geometry_msgs::TwistStamped::ConstPtr& msg )
+{/*{{{*/
+	ROS_INFO( "twist callback" );
+	double vel_x = msg->twist.linear.x;
+	double vel_y = msg->twist.linear.y;
+	robot_pose2d_.theta = atan2( vel_y, vel_x );
+}/*}}}*/
+
+void GnssFilter::pointCallback( const geometry_msgs::PointStamped::ConstPtr& msg )
+{/*{{{*/
+	ROS_INFO( "point callback" );
+	header_ = msg->header;
+	robot_pose2d_.x = msg->point.x;
+	robot_pose2d_.y = msg->point.y;
+	publishPose();
+	broadcastTF();
 }/*}}}*/
 
 void GnssFilter::doFiltering( const geometry_msgs::PoseStamped::ConstPtr& msg )
 {/*{{{*/
 	ROS_INFO( "do filtering" );
+	removeOldPose( msg->header.stamp );
+	motionUpdate( msg->header.stamp );
+	updatePoselist( *msg );
+	publishParticleCloud( msg->header );
 }/*}}}*/
 
-void GnssFilter::updatePoselist()
+void GnssFilter::removeOldPose( const ros::Time stamp )
 {/*{{{*/
-  static geometry_msgs::TransformStamped transform;
-  static geometry_msgs::TransformStamped transform_past;
-	static bool is_first_time = false;
+	std::vector<Pose2dWithTime> new_poselist;
+	for ( const auto& pose : poselist_ ) {
+		if ( stamp - pose.timestamp < ros::Duration( store_sec_ ) ) {
+			new_poselist.push_back( pose );
+		}
+	}
+	poselist_ = new_poselist;
+}/*}}}*/
+
+void GnssFilter::motionUpdate( const ros::Time stamp )
+{/*{{{*/
+	static geometry_msgs::TransformStamped tf;
+	static geometry_msgs::TransformStamped tf_past;
+	static bool is_first_time = true;
 
 	// get odom -> robot tf
 	try {
-		transform = tf2_buffer_.lookupTransform( odom_frame_, robot_frame_, ros::Time(0), ros::Duration( 0.1 ) );
-		is_first_time = true;
+		tf = tf2_buffer_.lookupTransform( odom_frame_, robot_frame_, stamp, ros::Duration( 0.1 ) );
 	}
 	catch ( tf2::TransformException &ex ) {
 		ROS_INFO( "[GnssFilter] cannot lookup tf ( from %s to %s )", odom_frame_.c_str(), robot_frame_.c_str() );
@@ -33,12 +78,120 @@ void GnssFilter::updatePoselist()
 		return;
 	}
 
-	if ( is_first_time ) return;
-	
-	transform_past = transform;
+	if ( is_first_time ) {
+		is_first_time = false;
+		return;
+	}
+
+	double diff_x = tf.transform.translation.x - tf_past.transform.translation.x;
+	double diff_y = tf.transform.translation.y - tf_past.transform.translation.y;
+	double r = sqrt( diff_x * diff_x + diff_y * diff_y );
+	double theta_gl = atan2( diff_y, diff_x );
+	double theta_p1 = tf::getYaw( tf_past.transform.rotation );
+	double theta_p2 = tf::getYaw( tf.transform.rotation );
+	double theta = theta_gl - theta_p1;
+	double motion_theta = theta_p2 - theta_p1;
+
+	for ( auto& pose : poselist_ ) {
+		double motion_x = r * cos( pose.pose2d.theta + theta );
+		double motion_y = r * sin( pose.pose2d.theta + theta );
+		pose.pose2d.x += motion_x;
+		pose.pose2d.y += motion_y;
+		pose.pose2d.theta += motion_theta;
+	}
+
+	tf_past = tf;
 }/*}}}*/
 
-void GnssFilter::spin()
+void GnssFilter::updatePoselist( const geometry_msgs::PoseStamped& measured_pose )
 {/*{{{*/
-	ros::spin();
+	if ( poselist_.size() < min_store_n_ ) {
+		Pose2dWithTime new_pose;
+		new_pose.pose2d.x = measured_pose.pose.position.x;
+		new_pose.pose2d.y = measured_pose.pose.position.y;
+		new_pose.pose2d.theta = tf::getYaw( measured_pose.pose.orientation );
+		new_pose.timestamp = measured_pose.header.stamp;
+		poselist_.push_back( new_pose );
+		return;
+	}
+
+	// calc mean
+	Eigen::Vector2d mean = Eigen::Vector2d::Zero();
+	for ( auto& pose : poselist_ ) {
+		mean.x() += pose.pose2d.x;
+		mean.y() += pose.pose2d.y;
+	}
+	mean /= poselist_.size();
+	gnss_mean = mean;
+
+	// calc standard deviation
+	double standard_deviation = 0.0;
+	for ( auto& pose : poselist_ ) {
+		double diff_x = mean.x() - pose.pose2d.x;
+		double diff_y = mean.y() - pose.pose2d.y;
+		standard_deviation += diff_x * diff_x + diff_y * diff_y;
+	}
+	standard_deviation = sqrt( standard_deviation / poselist_.size() );
+	gnss_sd = standard_deviation;
+
+	// calc measured_pose error
+	Eigen::Vector2d measured_p;
+	measured_p.x() = measured_pose.pose.position.x;
+	measured_p.y() = measured_pose.pose.position.y;
+	double error = ( mean - measured_p ).norm();
+
+	// add new pose if error is min
+	if ( error < gnss_sd * sd_threshold_ratio_ ) {
+		Pose2dWithTime new_pose;
+		new_pose.timestamp = measured_pose.header.stamp;
+		new_pose.pose2d.x = measured_pose.pose.position.x;
+		new_pose.pose2d.y = measured_pose.pose.position.y;
+		new_pose.pose2d.theta = tf::getYaw( measured_pose.pose.orientation );
+		poselist_.push_back( new_pose );
+	}
 }/*}}}*/
+
+void GnssFilter::publishParticleCloud( const std_msgs::Header header )
+{/*{{{*/
+	static int seq = 0;
+	geometry_msgs::PoseArray pose_array_msg;
+	pose_array_msg.header = header;
+
+	for ( const auto& pose : poselist_ ) {
+		geometry_msgs::Pose a_pose;
+		a_pose.position.x = pose.pose2d.x;
+		a_pose.position.y = pose.pose2d.y;
+		a_pose.position.z = 0.0;
+		geometry_msgs::Quaternion q;
+		quaternionTFToMsg( tf::createQuaternionFromRPY(0.0, 0.0, pose.pose2d.theta), q );
+		a_pose.orientation = q;
+		pose_array_msg.poses.push_back( a_pose );
+	}
+
+	pose_cloud_publisher_.publish( pose_array_msg );
+}/*}}}*/
+
+void GnssFilter::publishPose()
+{/*{{{*/
+	geometry_msgs::PoseStamped msg;
+	msg.header = header_; 
+	msg.pose.position.x = robot_pose2d_.x;
+	msg.pose.position.y = robot_pose2d_.y;
+	msg.pose.position.z = 0.0;
+	geometry_msgs::Quaternion q;
+	quaternionTFToMsg( tf::createQuaternionFromRPY(0.0, 0.0, robot_pose2d_.theta), q );
+	msg.pose.orientation = q;
+	robot_pose_publisher_.publish( msg );
+}/*}}}*/
+
+void GnssFilter::broadcastTF()
+{/*{{{*/
+	static tf::TransformBroadcaster br;
+	tf::Transform transform;
+	transform.setOrigin( tf::Vector3( robot_pose2d_.x, robot_pose2d_.y, 0.0 ) );
+	tf::Quaternion q;
+	q.setRPY( 0.0, 0.0, robot_pose2d_.theta );
+	transform.setRotation(q);
+	br.sendTransform( tf::StampedTransform( transform, header_.stamp, gnss_frame_, robot_gnss_frame_ ) );
+}/*}}}*/
+
